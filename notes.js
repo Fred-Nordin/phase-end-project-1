d77243aee3caa82e16ae6a475659b25bdbe74ef0
Fred-Nordin <162755361+Fred-Nordin@users.noreply.github.com>
2025-01-05 23:31:12 +0100

    Notes.

---
+++ notes.js
@@ -0,0 +1,17 @@
+// 1. Varibles
+Var name = “Fredrik”; // Kan få nytt värde
+Let ager2 = 38; // block-scoped, måste definieras innan användning.
+Const country = “Sweden” // Kan inte få nytt värde
+
+
+// 2. DATATYPES
+// let {} = Object
+let names = "Fredrik";
+let age = 43;
+let hobbies = ["Öl","Mat","barnen","Frun"];
+let person = {
+    name2: "Fredrik",
+    age: 38,
+};
+
+// 3
